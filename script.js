14fcd1718ce4111e3043f207b76431b6f472a7f9
Keerthana B <160339708+keerthanaskkun@users.noreply.github.com>
2025-07-01 22:12:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function checkURL() {
 
   // Check if input is empty
   if (!url) {
-    result.innerText = "⚠️ Please enter a URL first!";
+    result.innerText = " Please enter a URL first!";
     result.style.color = "orange";
     return;
   }
@@ -12,7 +12,7 @@ function checkURL() {
   // (Optional) Check if the format is valid
   const isValidUrl = /^(http|https):\/\/[^ "]+$/.test(url);
   if (!isValidUrl) {
-    result.innerText = "⚠️ Please enter a valid URL (start with http or https)";
+    result.innerText = " Please enter a valid URL (start with http or https)";
     result.style.color = "orange";
     return;
   }
@@ -28,15 +28,15 @@ function checkURL() {
     .then(res => res.json())
     .then(data => {
       if (data.prediction === 1) {
-        result.innerText = "❌ This is a phishing URL!";
+        result.innerText = "This is a phishing URL!";
         result.style.color = "red";
       } else {
-        result.innerText = "✅ This URL seems safe.";
+        result.innerText = "This URL seems safe.";
         result.style.color = "green";
       }
     })
     .catch(() => {
-      result.innerText = "⚠️ Server error. Please try again.";
+      result.innerText = "Server error. Please try again.";
       result.style.color = "orange";
     });
 }
